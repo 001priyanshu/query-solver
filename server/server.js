1b5d94f29bcbca34ce37e07a12dd7ede90fefa27
Priyanshu Rajput <priyanshurajput822400@gmail.com>
2023-08-18 23:20:12 +0530

    routes and dummy data added

--- server/server.js
+++ server/server.js
@@ -10,10 +10,18 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use("*", cors());
 
+
 const userRoutes = require("./routes/user");
+const chatRoutes = require("./routes/chat");
+const messageRoutes = require("./routes/message");
 
 app.use('/api/user',userRoutes);
+app.use('/api/chat',chatRoutes);
+app.use('/api/message',messageRoutes);
     
+mongoose.connect(process.env.DATABASE_URL).then(() => {
+  console.log("Database connection is successfull");
+});
 
 const server = app.listen(PORT, () => {
   console.log("listening on port 8080");
