3fd11d2ad5fdf2cd20ea740100a5ba38468adb63
Priyanshu Rajput <priyanshurajput822400@gmail.com>
2023-08-18 23:20:58 +0530

    socktes added

--- server/server.js
+++ server/server.js
@@ -27,3 +27,29 @@ const server = app.listen(PORT, () => {
   console.log("listening on port 8080");
 });
 
+const io = require('socket.io')(server,{
+  pingTimeout:80000,
+  cors : {
+    origin : "http://localhost:3000"
+  }
+})
+
+io.on("connection",(socket)=>{
+   socket.on('setup',(userData)=>{
+    socket.join(userData?._id);
+    socket.emit("connected")
+   })
+   socket.on('join chat',(room)=>{
+    socket.join(room)
+   })
+
+   socket.on('new message',(newMessageReceived)=>{
+      var chat = newMessageReceived.chat;
+      if(!chat?.users) return console.log("chat.users not defined")
+      chat.users.forEach(user=>{
+         if(user?._id == newMessageReceived?.sender._id) return;
+         socket.in(user._id).emit("message received",newMessageReceived);
+       })
+   })
+
+})
