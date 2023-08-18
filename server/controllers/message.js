b82659dc732492e9b8df55024e871305ae1801f4
Priyanshu Rajput <priyanshurajput822400@gmail.com>
2023-08-18 23:07:46 +0530

    sending messeges api

--- server/controllers/message.js
+++ server/controllers/message.js
@@ -15,3 +15,30 @@ exports.getAllMessages = async (req, res) => {
     throw new Error(error.message);
   }
 };
+
+exports.sendMessage = async (req, res) => {
+  const { content, chatId, userId } = req.body;
+  var newMessage = {
+    sender: userId,
+    content: content,
+    chat: chatId,
+  };
+  try {
+    var message = await Message.create(newMessage);
+
+    message = await message.populate("sender", "userId isAgent");
+    message = await message.populate("chat");
+    message = await User.populate(message, {
+      path: "chat.users",
+      select: "userId isAgent",
+    });
+
+    await Chat.findByIdAndUpdate(chatId, { latestMessage: message });
+
+    return res.send(message);
+  } catch (error) {
+    res.status(400)
+    throw new Error(error.message);
+  }
+};
+
