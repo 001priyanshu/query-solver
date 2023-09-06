65a72cf1cfb023e22ea3a3ed1440767f7f10185d
Priyanshu Rajput <priyanshurajput822400@gmail.com>
2023-09-06 23:44:52 +0530

    request a query page added

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import Landing from "./pages/Home";
 import SignInCustomer from "./pages/customers/SignIn";
 import SignupCustomer from "./pages/customers/Signup";
 import SignInAgent from './pages/agents/Signin'
-import SignupAgent from "./pages/agents/SingUp";
+// import SignupAgent from "./pages/agents/SingUp";
+import RaiseQuery from "./pages/customers/RaiseQuery";
 
 
 function App() {
@@ -14,7 +15,8 @@ function App() {
         <Route path="/logincustomer" exact element={<SignInCustomer />} />
         <Route path="/customersignup" exact element={<SignupCustomer/>} />
         <Route path="/loginagent" exact element={<SignInAgent/>} />
-        <Route path="/agentsignup" exact element={<SignupAgent/>} />
+        {/* <Route path="/agentsignup" exact element={<SignupAgent/>} /> */}
+        <Route path="/raisequery" exact element={<RaiseQuery />} />
 
 
       </Routes>
