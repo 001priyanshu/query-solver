870f77fc4356db288e3e76410cca98f9e1b0716b
Priyanshu Rajput <priyanshurajput822400@gmail.com>
2023-09-06 23:31:38 +0530

    signin of customer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Home";
+import SignInCustomer from "./pages/customers/SignIn";
+import SignupCustomer from "./pages/customers/Signup";
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/logincustomer" exact element={<SignInCustomer />} />
+        <Route path="/customersignup" exact element={<SignupCustomer/>} />
+
+
       </Routes>
     </Router>
   );
---
+++ client/src/actions/auth.jsx
@@ -0,0 +1,38 @@
+const api = 'http://localhost:8080/api'
+
+
+export const signupUser = async (userId,isAgent) => {
+    try {
+        const response = await fetch(`${api}/user/registeruser`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ userId: userId, isAgent : isAgent })
+        })
+        const result = await response.json();
+        return result;
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+export const loginCustomer = async (userId) => {
+    try {
+        const response = await fetch(`${api}/user/logincustomer`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ customerId: userId })
+        })
+        const result = await response.json();
+        return result;
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
