35ab08517048ebb4b0cd53b0bd26beb07ebe94df
Priyanshu Rajput <priyanshurajput822400@gmail.com>
2023-09-07 22:43:36 +0530

    users chatbox included

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes,Route } from "react-router-dom";
 import Landing from "./pages/Home";
 import SignInCustomer from "./pages/customers/SignIn";
-import SignupCustomer from "./pages/customers/Signup";
-import SignInAgent from './pages/agents/Signin'
-// import SignupAgent from "./pages/agents/SingUp";
 import RaiseQuery from "./pages/customers/RaiseQuery";
-
-
+import CustomerChatbox from "./pages/customers/ChatBox";
+import SignInAgent from "./pages/agents/Signin";
+import SignupCustomer from "./pages/customers/Signup";
+import SignupAgent from "./pages/agents/Singup";
 function App() {
   return (
-    <Router>
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/logincustomer" exact element={<SignInCustomer />} />
-        <Route path="/customersignup" exact element={<SignupCustomer/>} />
-        <Route path="/loginagent" exact element={<SignInAgent/>} />
-        {/* <Route path="/agentsignup" exact element={<SignupAgent/>} /> */}
-        <Route path="/raisequery" exact element={<RaiseQuery />} />
-
-
+          <Route path="/" exact element={<Landing />} />
+          <Route path="/logincustomer" exact element={<SignInCustomer />} />
+          <Route path="/raisequery" exact element={<RaiseQuery />} />
+          <Route path="/customerchatbox" exact element={<CustomerChatbox />} />
+          <Route path="/loginagent" exact element={<SignInAgent/>} />
+          <Route path="/customersignup" exact element={<SignupCustomer/>} />
+          <Route path="/agentsignup" exact element={<SignupAgent/>} />
       </Routes>
-    </Router>
   );
 }
 
